e751882c504b1e737583ba5d395270937303a61c
Jonathan Forney <jonathan.forney@gmail.com>
2019-12-13 18:01:43 -0800

    added navbar comp and react router imports

--- src/components/mainbody/MainBody.js
+++ src/components/mainbody/MainBody.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import Header from '../header/Header';
+import NavBar from '../navbar/NavBar';
 import Home from '../home/Home';
+import Projects from '../projects/Projects';
 import styles from './mainbody.css';
 
 function MainBody() {
   return (
     <mainbody className={styles.mainbody}>
       <Header />
-      <Home />
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/projects" component={Projects} />
+        </Switch>
+      </Router>
       Here is stuff
     </mainbody>
   );
