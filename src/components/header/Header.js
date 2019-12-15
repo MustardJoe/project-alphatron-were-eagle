109eee7a7a516d6101c23f9e560f24f97ee959ab
Jonathan Forney <jonathan.forney@gmail.com>
2019-12-15 15:34:16 -0800

    got header curvy radius working

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,13 +6,15 @@ function Header() {
   let topMsg = randomHeaderMsg();
 
   return (
-    <header className={styles.header}>
-      <a href="./">
-        <h1>
-            Hi! <span className={styles.difFont}>I&apos;m Jon Forney.</span> {topMsg}
-        </h1>
-      </a>
-    </header>
+    <div className={styles.wayback}>
+      <header className={styles.header}>
+        <a href="./">
+          <h1>
+              Hi! <span className={styles.difFont}>I&apos;m Jon Forney.</span> {topMsg}
+          </h1>
+        </a>
+      </header>
+    </div>
   );
 }
 
