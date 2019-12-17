d894137b8e91f04135fc537ef15cb62c574fa277
Jonathan Forney <jonathan.forney@gmail.com>
2019-12-16 19:21:17 -0800

    added more styling to links

--- src/components/links/Links.js
+++ src/components/links/Links.js
@@ -1,33 +1,40 @@
 import React from 'react';
+import Footer from '../footer/Footer';
 import styles from './links.css';
 
 function Links() {
   return (
-    <div class="hidden soc-media">
-      <div className={styles.spacerDiv}></div>
-      <div className={styles.twitterTag}>
+    <>
+      <div className={styles.socMedia}>
+        <div className={styles.spacerDiv}></div>
+        <div className={styles.twitterTag}>
         Feel free to contact me with your professional opportunities!
-      </div>
-      <div class="img-links-div">
-        <a href="https://github.com/MustardJoe" class="img-links" target="blank">
-          <img src="./assets/github-grn2.png" alt="GitHub icon"></img>
-        </a>
-        <a href="https://www.linkedin.com/in/jonathan-forney-52431a79/" class="img-links" target="blank">
-          <img src="./assets/linkedin2.png" alt="linked in icon"></img>
-        </a>
-        <a href="https://twitter.com/GodzillaJonny" class="img-links" target="blank">
-          <img src="./assets/twittergood.png" alt="linked in icon"></img>
-        </a>
-      </div>
-      <div class="spacer-div"></div>
-      <div class="cartoon-ref">
-        <img src="./assets/jonvatar.png"></img>
-        <p class="maya-ref">
-        Cartoon Jon avatar by <a href="https://www.mayacastillo.com/" target="blank">
+        </div>
+        <div className={styles.imgLinksDiv}>
+          <a href="https://github.com/MustardJoe" className={styles.imgLinks} target="blank">
+            <img src="./assets/github-grn2.png" alt="GitHub icon"></img>
+          </a>
+          <a href="https://www.linkedin.com/in/jonathan-forney-52431a79/" className={styles.imgLinks} 
+            target="_blank" rel="noopener noreferrer">
+            <img src="./assets/linkedin2.png" alt="linked in icon"></img>
+          </a>
+          <a href="https://twitter.com/GodzillaJonny" className={styles.imgLinks} target="_blank"
+            rel="noopener noreferrer">
+            <img src="./assets/twittergood.png" alt="linked in icon"></img>
+          </a>
+        </div>
+        <div className={styles.spacerDiv}></div>
+        <div className={styles.cartoonRef}>
+          <img src="./assets/jonvatar.png"></img>
+          <p className={styles.mayaRef}>
+        Cartoon Jon avatar by <a href="https://www.mayacastillo.com/" target="_blank"
+              rel="noopener noreferrer">
         Maya Castillo</a>
-        </p>
+          </p>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
