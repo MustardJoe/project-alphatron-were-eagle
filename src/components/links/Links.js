5093b7f98a36fda84f273158ad7a2a5f11d92ed7
Jonathan Forney <jonathan.forney@gmail.com>
2019-12-18 21:52:12 -0800

    updated img paths

--- src/components/links/Links.js
+++ src/components/links/Links.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Footer from '../footer/Footer';
 import styles from './links.css';
+import githubIcon from '../../assets/github-grn2.png';
+import linkedinIcon from '../../assets/linkedin2.png';
 import twitterIcon from '../../assets/twittergood.png';
+import miniAvatar from '../../assets/jonvatar.png';
 
 function Links() {
   return (
@@ -9,15 +12,15 @@ function Links() {
       <div className={styles.socMedia}>
         <div className={styles.spacerDiv}></div>
         <div className={styles.twitterTag}>
-        Feel free to contact me with your professional opportunities!
+          Feel free to contact me with your professional opportunities!
         </div>
         <div className={styles.imgLinksDiv}>
           <a href="https://github.com/MustardJoe" className={styles.imgLinks} target="blank">
-            <img src="../../assets/github-grn2.png" alt="GitHub icon"></img>
+            <img src={githubIcon} alt="GitHub icon"></img>
           </a>
           <a href="https://www.linkedin.com/in/jonathan-forney-52431a79/" className={styles.imgLinks} 
             target="_blank" rel="noopener noreferrer">
-            <img src="./src/assets/linkedin2.png" alt="Linked In icon"></img>
+            <img src={linkedinIcon} alt="LinkedIn icon"></img>
           </a>
           <a href="https://twitter.com/GodzillaJonny" className={styles.imgLinks} target="_blank"
             rel="noopener noreferrer">
@@ -26,11 +29,11 @@ function Links() {
         </div>
         <div className={styles.spacerDiv}></div>
         <div className={styles.cartoonRef}>
-          <img src="./src/assets/jonvatar.png"></img>
+          <img src={miniAvatar}></img>
           <p className={styles.mayaRef}>
-        Cartoon Jon avatar by <a href="https://www.mayacastillo.com/" target="_blank"
+            Cartoon Jon avatar by <a href="https://www.mayacastillo.com/" target="_blank"
               rel="noopener noreferrer">
-        Maya Castillo</a>
+            Maya Castillo</a>
           </p>
         </div>
       </div>
