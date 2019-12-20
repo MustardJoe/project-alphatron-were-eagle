e0f369451e7adf8382fe2d9cd51cc645ee0e0ad1
Jonathan Forney <jonathan.forney@gmail.com>
2019-12-19 23:00:01 -0800

    changed msg to generate in parent and pass in here as props

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import randomHeaderMsg from '../../services/randomHeaderMsg';
+import PropTypes from 'prop-types';
 import styles from './header.css';
 
-function Header() {
-  let topMsg = randomHeaderMsg();
+function Header({ currentHeadMsg }) {
+  let compMsg = currentHeadMsg;
 
   return (
     <div className={styles.wayback}>
       <header className={styles.header}>
         <a href="./">
           <h1>
-              Hi! <span className={styles.difFont}>I&apos;m Jon Forney.</span> {topMsg}
+              Hi! <span className={styles.difFont}>I&apos;m Jon Forney.</span> {compMsg}
           </h1>
         </a>
       </header>
     </div>
   );
 }
+
+Header.propTypes = {
+  currentHeadMsg: PropTypes.string.isRequired,
+};
 
 export default Header;
