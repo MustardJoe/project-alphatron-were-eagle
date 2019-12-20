71d3ac535dd0fb3b7af1475f45e71d172b84d6f0
Jonathan Forney <jonathan.forney@gmail.com>
2019-12-19 23:00:30 -0800

    changed to class comp to use state for headerMsg

--- src/components/mainbody/MainBody.js
+++ src/components/mainbody/MainBody.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,22 +9,37 @@ import NavBar from '../navbar/NavBar';
 import Home from '../home/Home';
 import Projects from '../projects/Projects';
 import Links from '../links/Links';
+import randomHeaderMsg from '../../services/randomHeaderMsg';
 import styles from './mainbody.css';
 
-function MainBody() {
-  return (
-    <div className={styles.mainbody}>
-      <Header />
-      <Router>
-        <NavBar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/projects" component={Projects} />
-          <Links path="/links" component={Links} />
-        </Switch>
-      </Router>
-    </div>
-  );
+let onLoadHeadMsg = randomHeaderMsg();
+
+class MainBody extends Component {
+  state = {
+    currentHeadMsg: onLoadHeadMsg,
+  }
+
+  updateHeadMsg = () => {
+    let newMsg = randomHeaderMsg();
+    return this.setState({ currentHeadMsg: newMsg });
+  }
+
+  render() {
+    const { currentHeadMsg } = this.state;
+    return (
+      <div className={styles.mainbody}>
+        <Header currentHeadMsg={currentHeadMsg} />
+        <Router>
+          <NavBar updateHeadMsg={this.updateHeadMsg}/>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/projects" component={Projects} />
+            <Links path="/links" component={Links} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default MainBody;
