219b2b6e413c08600ce0beed7e67edf102eec9fd
Jonathan Forney <jonathan.forney@gmail.com>
2019-12-19 23:00:55 -0800

    headerMsg updates w/each link click

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   Link
 } from 'react-router-dom';
 import styles from './navbar.css';
 
-function NavBar() {
+function NavBar({ updateHeadMsg }) {
   return (
     <nav className={styles.navBar}>
       <div className={styles.linkContainer}>
 
-        <Link to="/" className={styles.link}>Home</Link>
-        <Link to="/projects" className={styles.link}>Projects</Link>
-        <Link to="/links" className={styles.link}>Links/Refs</Link>
+        <Link to="/" className={styles.link} onClick={updateHeadMsg}>Home</Link>
+        <Link to="/projects" className={styles.link} onClick={updateHeadMsg}>Projects</Link>
+        <Link to="/links" className={styles.link} onClick={updateHeadMsg}>Links/Refs</Link>
       </div>
     </nav>
   );
 }
+
+NavBar.propTypes = {
+  updateHeadMsg: PropTypes.func.isRequired,
+};
 
 export default NavBar;
