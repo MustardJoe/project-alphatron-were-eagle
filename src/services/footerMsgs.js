b2e06ef259853fae78184d08a451f64786417761
Jonathan Forney <jonathan.forney@gmail.com>
2020-03-26 16:50:02 -0700

    💝new footer msg

--- src/services/footerMsgs.js
+++ src/services/footerMsgs.js
@@ -1,6 +1,7 @@
 function footerMsg() {
   const footerMsgs = [
     'The unexamined code is not worth writing.',
+    'Please be kind to others and remember to be kind you yourself as well.',
     'You\'ve made it to the bottom of the page! Congratulations!',
     'It\'s been a long day & you deserve a break!',
     'You\'ve reached level 999',
